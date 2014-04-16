9d51842b260b581049149a87c30a2deeaa4a4a26
Hani Suleiman <hani@formicary.net>
2014-04-16 16:04:58 -0400

    Better check for cucumber api

--- cucumber-scala/src/main/java/net/formicary/scala/CucumberScalaUtil.java
+++ cucumber-scala/src/main/java/net/formicary/scala/CucumberScalaUtil.java
@@ -1,5 +1,8 @@
 package net.formicary.scala;
 
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern;
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression;
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall;
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression;
@@ -13,12 +16,15 @@ public class CucumberScalaUtil {
   public static boolean isStepDefinition(ScMethodCall method) {
     ScExpression invokedExpr = method.getInvokedExpr();
     if(invokedExpr instanceof ScReferenceExpression) {
-    //todo surely there's a better way to check that these are methods from the DSL rather than just string checks
-      if(invokedExpr.getText().equals("When") 
-        || invokedExpr.getText().equals("Then") 
-        || invokedExpr.getText().equals("And") 
-        || invokedExpr.getText().equals("Given")) {
-        return true;
+      PsiElement resolved = ((ScReferenceExpression)invokedExpr).resolve();
+      if(resolved instanceof ScReferencePattern) {
+        PsiClass containingClass = ((ScReferencePattern)resolved).getContainingClass();
+        if(containingClass != null) {
+          String qualifiedName = containingClass.getQualifiedName();
+          if(qualifiedName != null && qualifiedName.startsWith("cucumber.api.scala")) {
+            return true;
+          }
+        }
       }
     }
     return false;
